54000cad30d675560d4e64d1565e2c99939915f2
DenisMaiyo <denismaiyo14@gmail.com>
2025-05-12 08:25:49 +0300

    added game data file

--- lib/basket_ball_functions.py
+++ lib/basket_ball_functions.py
@@ -1,23 +1,8 @@
+from lib.game_data import GAME_DATA
+
 def game_dict():
-    # Define the structure of the game dictionary
-    return {
-        "home": {
-            "team_name": "Home Team",
-            "colors": ["Red", "White"],
-            "players": [
-                {"name": "Player A", "number": 1, "points_per_game": 10, "age": 25, "shoe_brand": "Nike", "rebounds_per_game": 5},
-                {"name": "Player B", "number": 2, "points_per_game": 15, "age": 28, "shoe_brand": "Adidas", "rebounds_per_game": 7}
-            ]
-        },
-        "away": {
-            "team_name": "Away Team",
-            "colors": ["Blue", "Black"],
-            "players": [
-                {"name": "Player C", "number": 3, "points_per_game": 12, "age": 30, "shoe_brand": "Puma", "rebounds_per_game": 6},
-                {"name": "Player D", "number": 4, "points_per_game": 8, "age": 22, "shoe_brand": "Reebok", "rebounds_per_game": 4}
-            ]
-        }
-    }
+    # Return the imported game data dictionary
+    return GAME_DATA
 
 def get_all_players():
     game = game_dict()
---
+++ lib/game_data.py
@@ -0,0 +1,18 @@
+GAME_DATA = {
+    "home": {
+        "team_name": "Home Team",
+        "colors": ["Red", "White"],
+        "players": [
+            {"name": "Player A", "number": 1, "points_per_game": 10, "age": 25, "shoe_brand": "Nike", "rebounds_per_game": 5},
+            {"name": "Player B", "number": 2, "points_per_game": 15, "age": 28, "shoe_brand": "Adidas", "rebounds_per_game": 7}
+        ]
+    },
+    "away": {
+        "team_name": "Away Team",
+        "colors": ["Blue", "Black"],
+        "players": [
+            {"name": "Player C", "number": 3, "points_per_game": 12, "age": 30, "shoe_brand": "Puma", "rebounds_per_game": 6},
+            {"name": "Player D", "number": 4, "points_per_game": 8, "age": 22, "shoe_brand": "Reebok", "rebounds_per_game": 4}
+        ]
+    }
+}
